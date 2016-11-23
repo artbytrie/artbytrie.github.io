f29d072f701b6f272ddf036a67b834dffa1c00aa
artbytrie <trinarina2310@gmail.com>
2016-11-24 05:56:56 +0600

    Simply put display to none instead of fadeOut

--- commons/js/index.js
+++ commons/js/index.js
@@ -6,9 +6,9 @@
 	$('ul.nav').on('click', 'li', function() {
 		$(active.removeClass('active')
 		        .find('a')
-		        .attr('href')).fadeOut(300);
+		        .attr('href')).css("display","none");
 		$((active = $(this)).addClass('active')
 		                    .find('a')
-		                    .attr('href')).fadeIn(1800);
+		                    .attr('href')).fadeIn(900);
 	});
 })();
